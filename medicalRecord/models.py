b413bbc9c2322702424f751e90728294a0e97a05
Leo Nardi <leo.nardi.borba@gmail.com>
2019-09-13 06:54:37 -0300

    change model can be null

--- medicalRecord/models.py
+++ medicalRecord/models.py
@@ -4,6 +4,6 @@ class PatientForm(models.Model):
     name = models.CharField(max_length=255)
     birth = models.DateField(auto_now_add=False)
     collection = models.DateField(auto_now_add=False)
-    delivery = models.DateField(auto_now_add=False)
+    delivery = models.DateField(null=True, blank=True)
     doctor = models.CharField(max_length=255)
     formId = models.CharField(max_length=255)
