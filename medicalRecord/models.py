b1ae5d161da3f496d3235e4b1d69735c301a67c7
Leo Nardi <leo.nardi.borba@gmail.com>
2019-09-13 07:09:32 -0300

    accept null

--- medicalRecord/models.py
+++ medicalRecord/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 class PatientForm(models.Model):
     name = models.CharField(max_length=255)
-    birth = models.DateField(auto_now_add=False)
-    collection = models.DateField(auto_now_add=False)
-    delivery = models.DateField(null=True, blank=True)
+    birth = models.DateField(default=None, blank=True, null=True)
+    collection = models.DateField(default=None, blank=True, null=True)
+    delivery = models.DateField(default=None, blank=True, null=True)
     doctor = models.CharField(max_length=255)
     formId = models.CharField(max_length=255)
